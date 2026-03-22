0c61eb4e04abd3f9fe03d3146c5bfbbfdc15862d
skander-mahbouli <skander.mahbouli@epfl.ch>
2026-03-22 04:08:55 +0100

    Fix: add Buffer polyfill for @ton/ton browser compatibility

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,6 @@
+import { Buffer } from 'buffer';
+(window as unknown as { Buffer: typeof Buffer }).Buffer = Buffer;
+
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
